c9f1dd4a4d5eb603462b45118f9a1c5be691cc87
Jesus Andre Pantoja Barba <al234620@alumnos.uacj.mx>
2026-04-28 21:07:28 -0600

    feat: practica final das

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import csv
 
 app = FastAPI(title="Clientes API")
 
-clientes = []
+clientes=[]
 
-# Leer CSV
+with open("data.csv", newline="", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        clientes.append(row)
 
 @app.get("/clientes")
-def listar_clientes():
-    pass
+def listar():
+    return clientes
 
 @app.post("/clientes")
-def agregar_cliente(data: dict):
+def crear(data:dict):
     pass
 
 @app.delete("/clientes/{id}")
-def eliminar_cliente(id:int):
-    pass
+def borrar(id:int):
+    clientes.pop(ids)
+    return {"ok":True}
