3c1f71ac387c8fab9763faf16cf3a3b2e80c957a
Marcos <marcos.csca@gmail.com>
2026-04-29 19:01:42 -0600

    feat: se agregaron los endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import csv
+from fastapi import HTTPException
 
 app = FastAPI(title="Menu API")
 
@@ -12,12 +13,28 @@ with open("data.csv", newline="", encoding="utf-8") as file:
 
 @app.get("/menu")
 def listar():
+    if menu == []:
+        raise HTTPException(status_code=404, detail="El menu esta vacio")
     return menu
-
+# - precio mayor a 0 OK
+# - id no repetido
+# - nombre obligatorio OK
 @app.post("/menu")
 def crear(data:dict):
-    pass
+    for item in menu:
+        if item["id"] == data["id"]:
+            raise HTTPException(status_code=400, detail="El id ya existe")
+    if data["precio"] <= 0:
+        raise HTTPException(status_code=400, detail="El precio debe ser mayor a 0")
+    if data["nombre"] == "":
+        raise HTTPException(status_code=400, detail="El nombre es obligatorio")
+    menu.append(data)
+    return data
 
 @app.delete("/menu/{id}")
 def eliminar(id:int):
-    pass
+    for item in menu:
+        if item["id"] == id:
+            menu.remove(item)
+            return {"message": "Producto eliminado"}
+    raise HTTPException(status_code=404, detail="El producto no existe")
