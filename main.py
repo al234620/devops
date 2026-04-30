4c9eee582eb21921f955a36959be112a83018099
Enrique Alejandro Pereda Meraz <al223121@alumnos.uacj.mx>
2026-04-29 19:06:27 -0600

    endpoit put empleados

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+import csv
 
 app = FastAPI(title="Empleados API")
+
+
 
 @app.get("/empleados")
 def listar():
@@ -12,4 +15,24 @@ def crear(data:dict):
 
 @app.put("/empleados/{id}")
 def editar(id:int,data:dict):
-    pass
+    csv_file = "data.csv"
+    updated_rows = []
+    found = False
+    with open(csv_file, mode="r", newline="", encoding="utf-8") as file:
+        reader = list(csv.reader(file))
+        
+        for row in reader:
+            if row[0] == str(id):
+                row[1] = data.get("nombre", row[1])
+                row[2] = data.get("puesto", row[2])
+                found = True
+            updated_rows.append(row)
+
+    if not found:
+        return {"error": "Empleado no encontrado"}
+
+    with open(csv_file, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerows(updated_rows)
+
+    return {"message": "Empleado actualizado exitosamente"}
