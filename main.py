4e67778ddf3a28b01fe6d9d2392418b289e64125
RaulEstebanAnilesMacias <al222802@alumnos.uacj.mx>
2026-04-29 18:59:51 -0600

    get de empleados

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+import csv
 
 app = FastAPI(title="Empleados API")
 
 @app.get("/empleados")
 def listar():
-    pass
+    empleados = []
+    with open("data.csv", 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            empleados.append(row)
+    return empleados
 
 @app.post("/empleados")
 def crear(data:dict):
